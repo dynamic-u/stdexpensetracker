bf473960be618f4ec797563e6a20cf58cc0f4f94
Prashant <bhaivirat986@gmail.com>
2026-07-13 15:31:01 +0530

    file structure

---
+++ main.py
@@ -0,0 +1,10 @@
+# main menu based on console based choices
+
+# while true:
+
+# if choice == 1,2,3 --->transaction.py(add,delect,view)
+# elif choice  == 4 ---->summary.py-
+# elif choice  == 5 ---> look into dashboard(transaction.py ka data ko chart,graph ke form mein dikhayega)
+# error handling daalna padega 
+
+# elif  choice == 6 ---->exit from program
